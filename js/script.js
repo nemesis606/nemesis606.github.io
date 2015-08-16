c668560d28a883ea985b6ea941537af650a7b0b9
nemesis606 <zxmaster01@gmail.com>
2015-08-16 14:56:26 -0300

    Novas atualizações do front do curriculo online.

--- js/script.js
+++ js/script.js
@@ -11,4 +11,18 @@ $(document).ready(function(){
             playType : "oneShotPolyphonic"
         }
     );
+
+    $(".mz-profile-image").easyAudioEffects(
+        {
+            ogg: "assets/sounds/chime.ogg",
+            mp3: "assets/sounds/chime.mp3",
+            eventType: "hover", // or "click"
+            playType: "oneShotPolyphonic"
+        }
+    );
+
+    $(".mz-skills-box").ready(function(e){
+        var effect = $(".mz-skills-box").attr("data-class");
+        console.log(effect);
+    });
 });
